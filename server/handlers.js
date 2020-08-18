d734afcd59eae67720dea30eb513b16e0f3b34af
ari-rosie <62956748+ari-rosie@users.noreply.github.com>
2020-08-18 16:16:15 -0400

    Exercise-2

--- server/handlers.js
+++ server/handlers.js
@@ -1,5 +1,33 @@
-'use strict';
+"use strict";
+const { MongoClient } = require("mongodb");
 
-const getSeats = async (req, res) => {};
+require("dotenv").config();
+const { MONGO_URI } = process.env;
+
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+const getSeats = async (req, res) => {
+  let start = 0,
+    end;
+
+  try {
+    const client = await MongoClient(MONGO_URI, options);
+    await client.connect();
+    const db = client.db("ticket_booker");
+
+    const r = await db.collection("seats").find().toArray();
+    r.length > 100 ? (end = 100) : (end = r.length);
+    r
+      ? res.status(200).json({ status: 200, data: r.slice(start, end) })
+      : res.status(404).json({ status: 404, message: "Data not found." });
+
+    client.close();
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 module.exports = { getSeats };
